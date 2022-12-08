2436d8fa8b5aec248c3eaf9f06f9e8d8f498ba6c
Gaurav07076 <103797867+Gaurav07076@users.noreply.github.com>
2022-12-08 06:34:35 +0530

    Create Ques2.java

---
+++ Ques2.java
@@ -0,0 +1,17 @@
+//Even Fibbonaci
+
+public class Ques2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int x = 1;  // Represents the current Fibonacci number being processed
+        int y = 2;  // Represents the next Fibonacci number in the sequence
+        while (x <= 4000000) {
+            if (x % 2 == 0)
+                sum += x;
+            int z = x + y;
+            x = y;
+            y = z;
+        }
+        System.out.println(sum);
+    }
+}
