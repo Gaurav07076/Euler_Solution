251d49427c630312c00bcc001e7962fe503517f3
Gaurav07076 <103797867+Gaurav07076@users.noreply.github.com>
2022-12-08 06:42:11 +0530

    Create Ques6.java

---
+++ Ques6.java
@@ -0,0 +1,22 @@
+public class Ques6{
+    public static void main(String[] args) {
+        long numberOfPrimes = 0;
+        int number = 1;
+        int max = 10000000;
+        boolean[] sieve = new boolean[max];
+        for ( int i = 2; i < max; i++ ) {
+            if ( sieve[i] == true ) continue;
+
+            numberOfPrimes++;
+
+            if ( numberOfPrimes == 10001 ) {
+                number = i;
+                break;
+            }
+
+            for ( int j = i+i; j < maxLimit; j += i )
+                sieve[j] = true;
+        }
+        System.out.println("10001st prime: "+ number);
+    }
+}
